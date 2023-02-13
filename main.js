945fa6e5d1e92ae27d9b70ea13ee81a0f9f9b83a
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-02-13 14:15:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ let cvn = document.getElementById("mycanvas");
 let ctx = cvn.getContext("2d");
 cvn.width = 800;
 cvn.height = 800;
+
+let xo = 400;
+let yo = 400;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, cvn.width, cvn.height);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(xo, yo, 7, 7);
+
+function colorChange(color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(xo, yo, 7, 7);
+}
+
+colorChange("blue");
