511994180709088d58348459e330e7e47525a69d
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-02-13 14:46:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,58 @@ cvn.height = 800;
 let xo = 400;
 let yo = 400;
 
+let rightkeypressed = false;
+let leftkeypressed = false;
+let upkeypressed = false;
+let downkeypressed = false;
+
+document.addEventListener("keydown", keydown);
+document.addEventListener("keyup", keyup);
+
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, cvn.width, cvn.height);
 
-ctx.fillStyle = "blue";
-ctx.fillRect(xo, yo, 7, 7);
+requestAnimationFrame(loop);
+function loop() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, cvn.width, cvn.height);
 
-function colorChange(color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(xo, yo, 7, 7);
+  colorChange("blue", xo, yo, 50, 50);
+
+  if (rightkeypressed === true) {
+    xo += 5;
+  }
+
+  requestAnimationFrame(loop);
 }
 
-colorChange("blue");
+// color
+function colorChange(color, x, y, width, height) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+
+// key commands
+function keydown() {
+  if (event.keyCode === 68) {
+    rightkeypressed = true;
+  } else if (event.keyCode === 65) {
+    leftkeypressed = true;
+  } else if (event.keyCode === 87) {
+    upkeypressed = true;
+  } else if (event.keyCode === 83) {
+    downkeypressed = true;
+  }
+}
+
+function keyup() {
+  if (event.keyCode === 68) {
+    rightkeypressed = false;
+  } else if (event.keyCode === 65) {
+    leftkeypressed = false;
+  } else if (event.keyCode === 87) {
+    upkeypressed = false;
+  } else if (event.keyCode === 83) {
+    downkeypressed = false;
+  }
+}
