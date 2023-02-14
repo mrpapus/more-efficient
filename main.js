a3c23c37592a86124339eddc2297900edf60d949
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-02-14 14:49:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ let cvn = document.getElementById("mycanvas");
 let ctx = cvn.getContext("2d");
 cvn.width = 800;
 cvn.height = 800;
-
+let shooty = 0;
+let shootx = 0;
 let xo = 400;
 let yo = 400;
-
+let num = 5;
 let rightkeypressed = false;
 let leftkeypressed = false;
 let upkeypressed = false;
 let downkeypressed = false;
-
+let fire = false;
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
 
@@ -22,15 +23,36 @@ function loop() {
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, cvn.width, cvn.height);
 
-  colorChange("blue", xo, yo, 50, 50);
-
+  colorChange("blue", xo, yo, 60, 60);
+  //keys
   if (rightkeypressed === true) {
     xo += 5;
+    colorChange("red", xo + 30, yo + 10, 60, 40);
+  }
+  if (leftkeypressed === true) {
+    xo -= 5;
+    colorChange("red", xo - 30, yo + 10, 60, 40);
+  }
+  if (upkeypressed === true) {
+    yo -= 5;
+    colorChange("red", xo + 10, yo - 30, 40, 60);
+  }
+  if (downkeypressed === true) {
+    yo += 5;
+    colorChange("red", xo + 10, yo + 40, 40, 60);
+  }
+
+  // bullet
+
+  if (fire === true) {
   }
 
   requestAnimationFrame(loop);
 }
-
+// //change direction of cannon
+// function cannondirect() {
+//   colorChange("red", (xo -= 20), (yo -= 20), 50, 50);
+// }
 // color
 function colorChange(color, x, y, width, height) {
   ctx.fillStyle = color;
@@ -47,6 +69,8 @@ function keydown() {
     upkeypressed = true;
   } else if (event.keyCode === 83) {
     downkeypressed = true;
+  } else if (event.keyCode === 70) {
+    fire = true;
   }
 }
 
@@ -59,5 +83,7 @@ function keyup() {
     upkeypressed = false;
   } else if (event.keyCode === 83) {
     downkeypressed = false;
+  } else if (event.keyCode === 70) {
+    fire = false;
   }
 }
